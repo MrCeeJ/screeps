9ebb5afe24571a14090a9212bd2c6e0a53a3fc61
unknown <ctongue@SL-WS-183.scottlogic.co.uk>
2016-10-10 17:19:51 +0100

    initial workers

---
+++ main.js
@@ -0,0 +1,18 @@
+var roleWorker = require('role.worker');
+var maxCreeps = 15;
+var currentCreeps = 0;
+var harvesterCount = 0;
+
+module.exports.loop = function () {
+    
+    currentCreeps = _(Game.creeps).size();
+    //Spawning
+    if (currentCreeps < maxCreeps) {
+        Game.spawns['Spawn1'].createCreep( [WORK, CARRY, MOVE], null,{location:1, working:false} );
+    }
+
+    for(var name in Game.creeps) {
+        var creep = Game.creeps[name];
+        roleWorker.run(creep);
+    }
+}
---
+++ role.worker.js
@@ -0,0 +1,55 @@
+var roleWorker = {
+
+    /** @param {Creep} creep **/
+    run: function(creep) {
+        
+        // Working but ran out of enegery
+	    if(creep.memory.working && creep.carry.energy == 0) {
+            creep.memory.working = false;
+            creep.say('empty :(');
+	    }
+	    // Not working but full of energy
+	    if(!creep.memory.working && creep.carry.energy == creep.carryCapacity) {
+	        creep.memory.working = true;
+	        if (creep.memory.location === 4) {
+	            creep.memory.location = 0;    
+	        } else  {
+	            creep.memory.location++;
+	        }
+	        creep.say('full :)');
+	    }
+        
+        // Working, has energy
+	    if(creep.memory.working) {
+	        var targets = creep.room.find(FIND_CONSTRUCTION_SITES);
+	        // Refil Spawn
+	        if (Game.spawns['Spawn1'].energy < Game.spawns['Spawn1'].energyCapacity) {
+	            if(creep.transfer(Game.spawns['Spawn1'], RESOURCE_ENERGY) == ERR_NOT_IN_RANGE) {
+                     creep.moveTo(Game.spawns['Spawn1']);
+                } 
+	        }
+	        // Build Stuff
+            else if(targets.length) {
+                if(creep.build(targets[0]) == ERR_NOT_IN_RANGE) {
+                    creep.moveTo(targets[0]);
+                }
+            }
+            // Upgrade Controller
+            else {
+                if(creep.upgradeController(creep.room.controller) == ERR_NOT_IN_RANGE) {
+                    creep.moveTo(creep.room.controller);
+                }
+            }
+	    }
+	    // Gathering energy
+	    else {
+	        var sources = creep.room.find(FIND_SOURCES);
+	        var location = creep.memory.location == 0 ? 0 : 1;
+            if(creep.harvest(sources[location]) == ERR_NOT_IN_RANGE) {
+                creep.moveTo(sources[location]);
+            }
+	    }
+	}
+};
+
+module.exports = roleWorker;
