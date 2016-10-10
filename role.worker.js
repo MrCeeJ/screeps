0b38aadd8510608fd3054453415d3e532252bd30
Craig Tongue <MrCeeJ>
2016-10-10 17:46:43 +0100

    typo

--- role.worker.js
+++ role.worker.js
@@ -26,7 +26,7 @@ var roleWorker = {
 	        var priorityTargets = [STRUCTURE_CONTAINER];
 	        var priorityFound = false;
 	        for (var t in targets) {
-	        	if (!priorityFound && priorityTargets.indexOf(t.structure_type)) {
+	        	if (!priorityFound && priorityTargets.indexOf(t.structureType)) {
 	        		priorityFound = true;
 	        		if (creep.transfer(t) == ERR_NOT_IN_RANGE) {
                     	creep.moveTo(t);
