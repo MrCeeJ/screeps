957f4e7d3cb5a827aa6e9241ff9ffbdb0617f875
Craig Tongue <mrceej@gmail.com>
2016-10-11 22:42:16 +0100

    Added worker

--- role.worker.js
+++ role.worker.js
@@ -19,7 +19,7 @@ var roleWorker = {
             var extensionsNeedingEnergy = creep.room.find(FIND_STRUCTURES, {
             	filter: (i) => i.structureType == STRUCTURE_EXTENSION &&
                    		i.energy < i.energyCapacity
-            });
+                });
 
             // Build Random Stuff
             if (targets.length) {
@@ -35,26 +35,15 @@ var roleWorker = {
                 }
             }
 
-            // Refil any engery containers
-            else if (containersNeedingEnergy.length) {
-				if (creep.transfer(containersNeedingEnergy[0], RESOURCE_ENERGY) == ERR_NOT_IN_RANGE) {
-                    creep.moveTo(containersNeedingEnergy[0]);
-                }
-            }
-
-            // Upgrade Controller
-            else {
-                if (creep.upgradeController(creep.room.controller) == ERR_NOT_IN_RANGE) {
-                    creep.moveTo(creep.room.controller);
-                }
-            }
         }
         // Gathering energy
         else {
-            var sources = creep.room.find(FIND_SOURCES);
-            var location = creep.memory.location == 0 ? 0 : 1;
-            if (creep.harvest(sources[location]) == ERR_NOT_IN_RANGE) {
-                creep.moveTo(sources[location]);
+            var sources = creep.room.find(FIND_STRUCTURES, {
+                filter: (i) => i.structureType == STRUCTURE_CONTAINER &&
+                        i.energy < i.energyCapacity*.25
+                });
+            if (creep.withdraw(sources[0], RESOURCE_ENERGY) == ERR_NOT_IN_RANGE) {
+                creep.moveTo(sources[0]);
             }
         }
     }
