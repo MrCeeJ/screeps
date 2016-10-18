fbebaff84083d62375c0e39e3500acd98fe801a0
MrCeeJ <mrceej@mrceej.com>
2016-10-18 14:16:01 +0100

    Working hotfixes

--- role.worker.js
+++ role.worker.js
@@ -52,7 +52,7 @@ var roleWorker = {
 
             const containers = creep.room.find(FIND_STRUCTURES, {
                 filter: (i) => (i.structureType == STRUCTURE_CONTAINER &&
-                                i.energy > creep.carryCapacity)
+                                i.store[RESOURCE_ENERGY] > creep.carryCapacity)
             });
 
             if (!containers.length) {
