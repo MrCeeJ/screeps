ed5d619986de1903175763d250dc44a53e54488a
MrCeeJ <mrceej@mrceej.com>
2016-10-18 13:33:48 +0100

    Mining hotfixes

--- settings.js
+++ settings.js
@@ -2,8 +2,8 @@ var settings = {
     maxStarterUpgraders : 1,
     maxStarterMiners : 6,
     maxCreeps : 7,
-    maxTransporters : 0,
-    maxUpgraders : 0,
+    maxTransporters : 1,
+    maxUpgraders : 1,
     currentCreeps : 0,
 };
 module.exports = settings;
