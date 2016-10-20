ef8e6e6d1f0cb8b64ff4f25d506061f00f69978d
MrCeeJ <mrceej@mrceej.com>
2016-10-20 12:45:10 +0100

    Fixed tower finder bug

--- main.js
+++ main.js
@@ -25,8 +25,7 @@ module.exports.loop = function () {
         }
     }
 
-    const towers =  _.(Game.structures)
-        .filter(s => s.structureType == STRUCTURE_TOWER);
+    const towers = _.filter(Game.structures, (structure) => structure.structureType == STRUCTURE_TOWER);
 
     _.forEach(towers, function(tower) {
        roleTower.run(tower);
