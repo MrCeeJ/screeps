6ceeedce36273b428744c92cce8d5f12eea7d558
MrCeeJ <mrceej@mrceej.com>
2016-10-25 16:24:35 +0100

    Debugging transporters

--- settings.js
+++ settings.js
@@ -9,7 +9,7 @@ var settings = {
         'E63S63': {
             energySources : [Game.flags['Flag1'].pos, Game.flags['Flag2'].pos],
             mineralSources : [Game.flags['Flag3']],
-            sourceContainerIDs : ['580f6b288c4693a4382fd0c1']
+            sourceContainerIDs : ['580f6b288c4693a4382fd0c1', '580f7870ff27e79264b0c4f2']
         }
     }
 
