07eef428c1dfc2d2fc651c5802bdf68f9e371661
Craig Tongue <mrceej@gmail.com>
2016-10-29 18:15:24 +0100

    DIsabled dying respawn

--- main.js
+++ main.js
@@ -118,8 +118,8 @@ module.exports.loop = function () {
                 }
             });
             if (dyingMiners.length) {
-                utils.logMessage("Spawning replacement for :" + dyingMiners[0]);
-                spawnReplacementMiner(dyingMiners[0]);
+                //utils.logMessage("Spawning replacement for :" + dyingMiners[0]);
+                //spawnReplacementMiner(dyingMiners[0]);
             }
         }
 
