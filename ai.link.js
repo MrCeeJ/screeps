776bfc904c038bbe39a1f79ea9af8ffa34a62c05
MrCeeJ <mrceej@mrceej.com>
2016-11-01 09:14:01 +0000

    Down to one worker

--- ai.link.js
+++ ai.link.js
@@ -13,12 +13,12 @@ const tower = {
             return true;
         } else if (link.cooldown == 0) {
             if (link.energy > 400) {
-                utils.logMessage('Transferring '+link.energy+' energy from ' + link.pos + " to " + Game.getObjectById(destinationId).pos);
+               // utils.logMessage('Transferring '+link.energy+' energy from ' + link.pos + " to " + Game.getObjectById(destinationId).pos);
                 link.transferEnergy(Game.getObjectById(destinationId));
                 return true;
             }
             else {
-                utils.logMessage('Insufficient energy for transfer! ('+link.energy+')');
+               // utils.logMessage('Insufficient energy for transfer! ('+link.energy+')');
                 return false;
             }
         }
