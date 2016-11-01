b3d1a6fe40cb7a041627dd1e19dcbcb242f2eac8
MrCeeJ <mrceej@mrceej.com>
2016-11-01 17:18:51 +0000

    Additional spawn logging

--- ai.worker.js
+++ ai.worker.js
@@ -40,7 +40,10 @@ const drone = {
 
     getBody: function (energy) {
 
-        if (energy >= 1000) {
+        if (energy >= 1200) {
+            return [WORK, WORK, WORK, WORK, WORK, WORK, CARRY, CARRY, CARRY, CARRY, CARRY, CARRY, MOVE, MOVE, MOVE, MOVE, MOVE, MOVE];
+        }
+        else if (energy >= 1000) {
             return [WORK, WORK, WORK, WORK, WORK, CARRY, CARRY, CARRY, CARRY, CARRY, MOVE, MOVE, MOVE, MOVE, MOVE];
         }
         else if (energy >= 800) {
