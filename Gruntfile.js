d4c300c78b04d6aa0e9b940dd63f4b4885321c11
Craig Tongue <ctongue@scottlogic.com>
2017-12-15 15:29:52 +0000

    Added gruntfile

---
+++ Gruntfile.js
@@ -0,0 +1,25 @@
+module.exports = function(grunt) {
+
+    grunt.loadNpmTasks('grunt-screeps');
+
+    grunt.initConfig({
+        screeps: {
+            options: {
+                email: 'mrceej@gmail.com',
+                password: '',
+                branch: 'default',
+                ptr: false
+            },
+            dist: {
+                files: [
+                    {
+                        expand: true,
+                        cwd: 'dist/',
+                        src: ['**/*.js'],
+                        flatten: true
+                    }
+                ]
+            }
+        }
+    });
+}
