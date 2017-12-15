fbc6b25a718d2324c65c783e8ac3463b5b5b3a82
Craig Tongue <ctongue@scottlogic.com>
2017-12-15 15:35:53 +0000

    update miner

--- Gruntfile.js
+++ Gruntfile.js
@@ -6,7 +6,7 @@ module.exports = function(grunt) {
         screeps: {
             options: {
                 email: 'mrceej@gmail.com',
-                password: 'q0Y5cet3tt6R',
+                password: '',
                 branch: 'default',
                 ptr: false
             },
